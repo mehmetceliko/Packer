95a6f297edfb8624a934ae618b56cd0894c9818d
mehmetceliko <mehmetceliko@gmail.com>
2021-05-24 01:46:52 +0300

    unit test

--- src/test/java/implementation/PackerTest.java
+++ src/test/java/implementation/PackerTest.java
@@ -1,6 +1,7 @@
 package implementation;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 import org.junit.Test;
 
@@ -11,28 +12,34 @@ public class PackerTest {
 
 	 	
 	
-//	@Test
-//	public void testPack() throws APIException {
-//		String fileAbsPath = "C:\\workspaces\\dev\\mobiquity\\implementation\\src\\test\\resources\\testFile2.txt";
-//		
-//		String result = Packer.pack(fileAbsPath); 
-//		
-//		System.out.println(result);
-//		
-//		assertNotNull(result);
-//		
-//	}
-	
-	
 	@Test
-	public void testPackError() throws APIException {
-		String fileAbsPath = "C:\\workspaces\\dev\\mobiquity\\implementation\\src\\test\\resources\\testFileError1.txt";
+	public void testPack() throws APIException {
+		String fileAbsPath = "C:\\workspaces\\dev\\mobiquity\\implementation\\src\\test\\resources\\testFile2.txt";
 		
 		String result = Packer.pack(fileAbsPath); 
 		
 		System.out.println(result);
 		
 		assertNotNull(result);
+		
+	}
+	
+	
+	@Test
+	public void testPackError() throws APIException {
+		String fileAbsPath = "C:\\workspaces\\dev\\mobiquity\\implementation\\src\\test\\resources\\testFileError1.txt";
+		
+		String result=null;
+		try {
+			result = Packer.pack(fileAbsPath);
+			System.out.println(result);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+			
+		
+		assertNull(result);
 		
 	}
 	
